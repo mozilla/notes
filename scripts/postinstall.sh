601fab9159079c2b23bdd05554095240ebac2596
Peter deHaan <peter@deseloper.com>
2017-06-16 09:16:21 -0700

    Use minified versions of vendor libraries

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,7 +3,8 @@
 set -e
 
 cp node_modules/testpilot-metrics/testpilot-metrics.js src/vendor
-cp node_modules/kinto-http/dist/kinto-http.js src/sidebar/vendor
-cp node_modules/quill/dist/quill.js src/sidebar/vendor
+
+cp node_modules/kinto-http/dist/kinto-http.min.js src/sidebar/vendor/kinto-http.js
+cp node_modules/quill/dist/quill.min.js src/sidebar/vendor/quill.js
 cp node_modules/quill/dist/quill.snow.css src/sidebar/vendor
-cp node_modules/jose-jwe-jws/dist/jose.js src/sidebar/vendor
+cp node_modules/jose-jwe-jws/dist/jose.min.js src/sidebar/vendor/jose.js
