877c19ecc16a7ba0e0ebee4efe3cd8349ca8b157
Peter deHaan <peter@deseloper.com>
2017-06-16 18:25:09 -0700

    Add vendor licenses for Jose and Kinto-HTTP

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,3 +7,8 @@ cp node_modules/kinto-http/dist/kinto-http.js src/sidebar/vendor
 cp node_modules/quill/dist/quill.js src/sidebar/vendor
 cp node_modules/quill/dist/quill.snow.css src/sidebar/vendor
 cp node_modules/jose-jwe-jws/dist/jose.js src/sidebar/vendor
+
+# Copy the 3rd party LICENSE files.
+cp node_modules/kinto-http/LICENSE src/sidebar/vendor/kinto-http.LICENSE
+cp node_modules/quill/LICENSE src/sidebar/vendor/quill.LICENSE
+cp node_modules/jose-jwe-jws/LICENSE src/sidebar/vendor/jose.LICENSE
