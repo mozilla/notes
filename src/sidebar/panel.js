994ebfd3507b8248b4ece14ce5850a7d0d2bdd80
Cedricium <amaya.cedric@gmail.com>
2017-07-01 23:26:10 -0700

    Titles and key-bindings for toolbar buttons
    This commit is a quick fix to issue #72 - missing tooltips. Currently, Quill
    has no support for native tooltips, rather relying on Bootstrap (which requires
    jQuery). For usuability purposes, I've added titles to the toolbar buttons
    which appear when hovering over the buttons, similar to a tooltip but not as
    styled. The titles themselves include the button's functionality ('Font size',
    'Bold', 'Bulleted list', etc.) as well as the key-binding associated with it.
    
    Currently, only the 'Bold' and 'Italic' formats included a default key-binding.
    Following the documentation for adding additional bindings to the keyboard
    module (found here: https://quilljs.com/docs/modules/keyboard/), I went ahead
    and added three bindings for the buttons/formats in the toolbar without them
    ('strike', 'list: ordered', 'list: bulleted'). By determining the user's operating
    system, the titles display the appropriate key for the binding sequence (either
    Mac's Cmd symbol or 'Ctrl').

--- src/sidebar/panel.js
+++ src/sidebar/panel.js
@@ -15,6 +15,42 @@ const RTL_LANGS = ['ar', 'fa', 'he'];
 const LANG_DIR = RTL_LANGS.includes(UI_LANG) ? 'rtl' : 'ltr';
 const TEXT_ALIGN_DIR = LANG_DIR === 'rtl' ? 'right' : 'left';
 
+// Additional keyboard shortcuts for non-default toolbar buttons
+const bindings = {
+  strike: {           // ⌘/Ctrl+Shift+X
+    key: 'X',
+    shiftKey: true,
+    shortKey: true,
+    handler: function(range, context) {
+      if (this.quill.getFormat(range).strike == true)
+        this.quill.formatText(range, 'strike', false);
+      else
+        this.quill.formatText(range, 'strike', true);
+    }
+  }, ordered: {       // ⌘/Ctrl+Shift+1
+    key: '1',
+    shiftKey: true,
+    shortKey: true,
+    handler: function(range, context) {
+      console.log(this.quill.getFormat(range));
+      if (this.quill.getFormat(range).list === "ordered")
+        this.quill.formatLine(range, {'list': false}, true);
+      else
+        this.quill.formatLine(range, {'list': "ordered"}, true);
+    }
+  }, bullet: {        // ⌘/Ctrl+Shift+2
+    key: '2',
+    shiftKey: true,
+    shortKey: true,
+    handler: function(range, context) {
+      console.log(this.quill.getFormat(range));
+      if (this.quill.getFormat(range).list === "bullet")
+        this.quill.formatLine(range, {'list': false}, true);
+      else
+        this.quill.formatLine(range, {'list': "bullet"}, true);
+    }
+  }
+}
 
 const fontSizeStyle = Quill.import('attributors/style/size');
 fontSizeStyle.whitelist = ['12px', '14px', '16px', '18px', '20px'];
@@ -30,10 +66,51 @@ const quill = new Quill('#editor', {
   theme: 'snow',
   placeholder: browser.i18n.getMessage('emptyPlaceHolder'),
   modules: {
+    keyboard: {
+      bindings: bindings
+    },
     toolbar: '#toolbar'
   },
   formats: formats // enabled formats, see https://github.com/quilljs/quill/issues/1108
 });
+
+
+function determineOS() {
+  var OSName = "";
+  
+  if (navigator.appVersion.indexOf("Win") != -1)
+    OSName = "win";
+  if (navigator.appVersion.indexOf("Mac") != -1)
+    OSName = "mac";
+  if (navigator.appVersion.indexOf("Linux") != -1)
+    OSName = "linux";
+  
+  return OSName;
+}
+
+const userOS = determineOS();
+var userOSKey = '';
+
+if (userOS === 'mac')
+  userOSKey = '⌘';
+else
+  userOSKey = 'Ctrl';
+
+var size = document.getElementsByClassName("ql-size")[0],
+    bold = document.getElementsByClassName("ql-bold")[0],
+    italic = document.getElementsByClassName("ql-italic")[0],
+    strike = document.getElementsByClassName("ql-strike")[0],
+    ordered = document.getElementsByClassName("ql-list")[0],
+    bullet = document.getElementsByClassName("ql-list")[1];
+
+// Setting button titles in place of tooltips
+size.title = "Font size",
+  bold.title = "Bold (" + userOSKey + "+B)",
+  italic.title = "Italic (" +userOSKey + "+I)",
+  strike.title = "Strikethrough (" + userOSKey + "+Shift+" + bindings.strike.key + ")",
+  ordered.title = "Numbered list (" + userOSKey + "+Shift+" + bindings.ordered.key + ")",
+  bullet.title = "Bulleted list (" + userOSKey + "+Shift+" + bindings.bullet.key + ")";
+
 
 function handleLocalContent(data) {
   if (!data.hasOwnProperty('notes')) {
