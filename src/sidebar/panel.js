08e22bc7dd51cceb9436e6d779d6fedd7413a449
Cedricium <amaya.cedric@gmail.com>
2017-07-04 15:39:17 -0700

    Revisions for PR #124
    - Added missing semi-colons to each line after setting the titles for every button
    - Added a title for the 'text-direction' button/format in `notes.properties`
    - Assigned 'text-direction' title to its corresponding button using
    `i18n.getMessage()`

--- src/sidebar/panel.js
+++ src/sidebar/panel.js
@@ -43,12 +43,13 @@ const size = document.getElementsByClassName('ql-size')[0],
       bullet = document.getElementsByClassName('ql-list')[1];
 
 // Setting button titles in place of tooltips
-size.title = browser.i18n.getMessage('fontSizeTitle')
-bold.title = browser.i18n.getMessage('boldTitle')
-italic.title = browser.i18n.getMessage('italicTitle')
-strike.title = browser.i18n.getMessage('strikethroughTitle')
-ordered.title = browser.i18n.getMessage('numberedListTitle')
-bullet.title = browser.i18n.getMessage('bulletedListTitle')
+size.title = browser.i18n.getMessage('fontSizeTitle');
+bold.title = browser.i18n.getMessage('boldTitle');
+italic.title = browser.i18n.getMessage('italicTitle');
+strike.title = browser.i18n.getMessage('strikethroughTitle');
+ordered.title = browser.i18n.getMessage('numberedListTitle');
+bullet.title = browser.i18n.getMessage('bulletedListTitle');
+qlDirection.title = browser.i18n.getMessage('textDirectionTitle');
 
 function handleLocalContent(data) {
   if (!data.hasOwnProperty('notes')) {
