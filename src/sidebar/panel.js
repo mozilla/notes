622e71cc1e24885db432687703a253eac74a96d6
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-07-10 10:06:16 +0200

    @natim review.

--- src/sidebar/panel.js
+++ src/sidebar/panel.js
@@ -97,7 +97,7 @@ quill.on('text-change', () => {
 
 const enableSync = document.getElementById('enable-sync');
 const noteDiv = document.getElementById('sync-note');
-const syncNoteBody = document.getElementById('sync-note-dialogue');
+const syncNoteBody = document.getElementById('sync-note-dialog');
 const closeButton = document.getElementById('close-button');
 enableSync.textContent = browser.i18n.getMessage('syncNotes');
 syncNoteBody.textContent = browser.i18n.getMessage('syncNotReady');
