063ea5215e04c7b7a9fc74d53a83fc03efcfd96a
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-10-06 17:31:06 -0400

    Add more tests to try to verify the handling of decryption

--- test/unit/main.test.js
+++ test/unit/main.test.js
@@ -9,6 +9,15 @@ chai.use(chaiAsPromised);
 // Many of these are "functional" tests that are run using Karma, and
 // so "unit" tests from the browser perspective (not including browser interaction).
 describe('Authorization', function() {
+  let sandbox;
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
   it('should define syncKinto', function() {
     chai.expect(syncKinto).not.eql(undefined);
   });
@@ -48,6 +57,34 @@ describe('Authorization', function() {
 
     it('should not reject the promise', function() {
       return chai.expect(syncKinto(client, credentials)).fulfilled;
+    });
+  });
+
+  describe("remote transformer", function() {
+    const kid = 20171005;
+    const key = {kid: kid, kty: "kty"};
+    it("should return whatever decrypt returns", function() {
+      const decryptedResult = { content: [{ insert: "Test message" }] };
+      const decryptMock = sandbox.stub(global, 'decrypt');
+      decryptMock.returns(decryptedResult);
+      chai.expect(new JWETransformer(key).decode({content: "encrypted content", kid: kid})).eventually.eql({
+        content: [{insert: "Test message"}],
+      });
+    });
+
+    it("should throw if kid is different", function() {
+      chai.expect(new JWETransformer(key).decode({content: "encrypted content", kid: 20171001})).rejectedWith(ServerKeyOlderError);
+    });
+
+    it("should be backwards compatible with the old style of Kinto record", function() {
+      const oldRecordStyle = [
+        { insert: "Test message" },
+      ];
+      const decryptMock = sandbox.stub(global, 'decrypt');
+      decryptMock.returns(oldRecordStyle);
+      chai.expect(new JWETransformer(key).decode({content: "encrypted content", kid: kid})).eventually.eql({
+        content: [{insert: "Test message"}],
+      });
     });
   });
 });
