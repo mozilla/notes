0b48d6c106223757e2d3a3fc87e89794d4790466
Cedricium <amaya.cedric@gmail.com>
2017-11-13 21:57:30 -0800

    (Tried) Building Classic editor from source
    This was an attempt to build the Classic editor from source as outlined
    in this guide: https://docs.ckeditor.com/ckeditor5/latest/builds/guide-
    s/integration/advanced-setup.html.
    
    This isn't bundled correctly however since ClassicEditor.js and
    Markdown.js are at the top-level of the directory and need to be inside
    the `sidebar/` directory.

---
+++ Markdown.js
@@ -0,0 +1,6 @@
+import GFMDataProcessor from '@ckeditor/ckeditor5-markdown-gfm/src/gfmdataprocessor';
+
+// Simple plugin which loads the data processor.
+export default function Markdown( editor ) {
+	editor.data.processor = new GFMDataProcessor();
+}
