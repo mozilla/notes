549bfee0dcd83256916cf63d3a7c612786a72d05
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-11-21 16:10:36 +0100

    Merge CKEditor with Sync feature.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,9 +4,6 @@ const { copySync } = require('fs-extra');
 
 const files = [
   copySync('node_modules/testpilot-ga/dist/index.js', 'src/vendor/testpilot-ga.js'),
-  copySync('node_modules/quill/dist/quill.min.js', 'src/sidebar/vendor/quill.js'),
-  copySync('node_modules/quill/dist/quill.snow.css', 'src/sidebar/vendor/quill.snow.css'),
-  copySync('node_modules/quill/LICENSE', 'src/sidebar/vendor/quill.LICENSE'),
   copySync('node_modules/kinto-http/dist/kinto-http.min.js', 'src/vendor/kinto-http.js'),
   copySync('node_modules/kinto-http/LICENSE', 'src/vendor/kinto-http.LICENSE'),
   copySync('node_modules/kinto/dist/kinto.noshim.js', 'src/vendor/kinto.js'),
@@ -16,7 +13,11 @@ const files = [
   copySync('node_modules/fxa-crypto-relier/dist/fxa-crypto-relier/fxa-crypto-relier.js', 'src/vendor/fxa-crypto-relier/fxa-crypto-relier.js'),
   copySync('node_modules/material-design-lite/material.min.js', 'src/sidebar/vendor/material.js'),
   copySync('node_modules/material-design-lite/material.min.css', 'src/sidebar/vendor/material.css'),
-  copySync('node_modules/material-design-lite/LICENSE', 'src/sidebar/vendor/material.LICENSE')
+  copySync('node_modules/material-design-lite/LICENSE', 'src/sidebar/vendor/material.LICENSE'),
+  copySync('node_modules/@ckeditor/ckeditor5-build-classic/LICENSE.md', 'src/sidebar/vendor/ckeditor.LICENSE'),
+  // Remove quill after migration
+  copySync('node_modules/quill/LICENSE', 'src/sidebar/vendor/quill.LICENSE'),
+  copySync('node_modules/quill/dist/quill.min.js', 'src/sidebar/vendor/quill.js')
 ];
 
 Promise.all(files).catch(err => {
--- src/sidebar/panel.js
+++ src/sidebar/panel.js
@@ -1,318 +1,9 @@
-const formats = [
-  'link',
-  'bold',
-  'font',
-  'italic',
-  'size',
-  'strike',
-  'indent',
-  'list',
-  'direction',
-  'align'
-];
+/* exported TEXT_ALIGN_DIR */
 const UI_LANG = browser.i18n.getUILanguage();
 const RTL_LANGS = ['ar', 'fa', 'he'];
 const LANG_DIR = RTL_LANGS.includes(UI_LANG) ? 'rtl' : 'ltr';
 const TEXT_ALIGN_DIR = LANG_DIR === 'rtl' ? 'right' : 'left';
 const SURVEY_PATH = 'https://qsurvey.mozilla.com/s3/notes?ref=sidebar';
-
-// Additional keyboard shortcuts for non-default toolbar buttons
-const bindings = {
-  strike: {           // ⌘/Ctrl+Shift+X
-    key: 'X',
-    shiftKey: true,
-    shortKey: true,
-    handler: function(range) {
-      if (this.quill.getFormat(range).strike === true)
-        this.quill.formatText(range, 'strike', false);
-      else
-        this.quill.formatText(range, 'strike', true);
-    }
-  }, ordered: {       // ⌘/Ctrl+Shift+1
-    key: '1',
-    shiftKey: true,
-    shortKey: true,
-    handler: function(range) {
-      if (this.quill.getFormat(range).list === 'ordered')
-        this.quill.formatLine(range, {'list': false}, true);
-      else
-        this.quill.formatLine(range, {'list': 'ordered'}, true);
-    }
-  }, bullet: {        // ⌘/Ctrl+Shift+2
-    key: '2',
-    shiftKey: true,
-    shortKey: true,
-    handler: function(range) {
-      if (this.quill.getFormat(range).list === 'bullet')
-        this.quill.formatLine(range, {'list': false}, true);
-      else
-        this.quill.formatLine(range, {'list': 'bullet'}, true);
-    }
-  }
-};
-
-const Block = Quill.import('blots/block');
-Block.tagName = 'DIV';
-Quill.register(Block, true);
-
-const fontSizeStyle = Quill.import('attributors/style/size');
-fontSizeStyle.whitelist = ['12px', '14px', '16px', '18px', '20px'];
-Quill.register(fontSizeStyle, true);
-
-// add text direction icon to toolbar if RTL
-const qlDirection = document.getElementById('ql-direction');
-if (LANG_DIR === 'rtl') {
-  qlDirection.innerHTML = '<button class="ql-direction" value="rtl"></button>';
-}
-
-const quill = new Quill('#editor', {
-  theme: 'snow',
-  placeholder: browser.i18n.getMessage('emptyPlaceHolder'),
-  modules: {
-    keyboard: {
-      bindings: bindings
-    },
-    toolbar: '#toolbar'
-  },
-  formats: formats // enabled formats, see https://github.com/quilljs/quill/issues/1108
-});
-
-function isWhitespace(ch) {
-  let whiteSpace = false;
-  if ((ch === ' ') || (ch === '\t') || (ch === '\n')) {
-    whiteSpace = true;
-  }
-  return whiteSpace;
-}
-
-// recognizes typed urls and create links from those urls
-quill.on('text-change', function(delta) {
-  const regex = /https?:\/\/[^\s]+$/;
-  if (delta.ops.length === 2 && delta.ops[0].retain ) {
-    let endRetain = delta.ops[0].retain;
-    if ('insert' in delta.ops[1]) {
-      endRetain += 1;
-    }
-    const text = quill.getText().substr(0, endRetain);
-    const format = quill.getFormat(text);
-    const match = text.match(regex);
-
-    if (match !== null) {
-      const url = match[0];
-
-      let ops = [];
-      if (endRetain > url.length) {
-        ops.push({ retain: endRetain - url.length });
-      }
-
-      const formats = {};
-      // apply any previous formatting options to the attributes object
-      Object.keys(format).forEach(function(key) {
-        formats[key] = format[key];
-      });
-      formats.link = url;
-
-      ops = ops.concat([
-        { delete: url.length },
-        { insert: url, attributes: formats }
-      ]);
-
-      quill.updateContents({
-        ops: ops
-      });
-    }
-  }
-});
-
-// recognizes pasted urls and create links from those urls
-quill.clipboard.addMatcher(Node.TEXT_NODE, function(node, delta) {
-  let formatsAtIndex = {};
-
-  const range = quill.getSelection(true);
-  if (range) {
-    if (range.length === 0) {
-      formatsAtIndex = quill.getFormat(range.index);
-    }
-  }
-
-  const regex = /https?:\/\/[^\s]+/;
-  if (typeof(node.data) !== 'string')
-    return;
-  const matches = node.data.match(regex);
-
-  if (matches === null) {
-    const ops = [];
-    const str = node.data;
-
-    ops.push({ insert: str, attributes: formatsAtIndex });
-    delta.ops = ops;
-  }
-
-  if (matches && matches.length > 0) {
-    const ops = [];
-    let str = node.data;
-
-    matches.forEach(function(match) {
-      const split = str.split(match);
-      const beforeLink = split.shift();
-      ops.push({ insert: beforeLink });
-
-      formatsAtIndex.link = match;
-
-      ops.push({ insert: match, attributes: formatsAtIndex });
-      str = split.join(match);
-    });
-
-    ops.push({ insert: str });
-    delta.ops = ops;
-  }
-
-  return delta;
-});
-
-function containsAnchor(el) {
-  if (el.tagName === 'A')
-    return el;
-
-  while (el.parentElement) {
-    el = el.parentElement;
-
-    if (el.tagName === 'A')
-      return el;
-  }
-}
-
-// adds an eventListener to every <a> element which opens their respective
-// href link in a new tab when clicked
-document.querySelector('#editor').addEventListener('click', function(e) {
-  let el = e.target;
-  el = containsAnchor(el);
-
-  if (el === undefined)
-    return;
-  else if (el !== null && el.tagName === 'A') {
-    browser.runtime.sendMessage({
-      action: 'link-clicked',
-      context: getPadStats()
-    });
-    browser.tabs.create({
-      active: true,
-      url: el.href
-    });
-  }
-});
-
-// makes getting out of link-editing format easier by escaping whitespace characters
-quill.on('text-change', function(delta) {
-  if (delta.ops.length === 2 && 'insert' in delta.ops[1] &&
-      isWhitespace(delta.ops[1].insert)) {
-    const format = quill.getFormat(delta.ops[0].retain, 1);
-    if ('link' in format)
-      quill.formatText(delta.ops[0].retain, 1, 'link', false);
-  }
-  // delta match when [Space] is used before link at beginning of document, fixes #273
-  else if (delta.ops.length === 1 && 'insert' in delta.ops[0]) {
-    if (isWhitespace(delta.ops[0].insert)) {
-      quill.formatText(0, 1, 'link', false);
-    }
-  }
-  // delta match when [Tab] is used before link at beginning of document, fixes #273
-  else if (delta.ops.length === 2 && 'insert' in delta.ops[0]) {
-    quill.formatText(0, delta.ops[1].retain + 1, 'link', false);
-  } else
-    return;
-});
-
-let userOSKey;
-
-if (navigator.appVersion.indexOf('Mac') !== -1)
-  userOSKey = '⌘';
-else
-  userOSKey = 'Ctrl';
-
-const size = document.getElementsByClassName('ql-size')[0],
-      bold = document.getElementsByClassName('ql-bold')[0],
-      italic = document.getElementsByClassName('ql-italic')[0],
-      strike = document.getElementsByClassName('ql-strike')[0],
-      ordered = document.getElementsByClassName('ql-list')[0],
-      bullet = document.getElementsByClassName('ql-list')[1];
-
-// Setting button titles in place of tooltips
-size.title = browser.i18n.getMessage('fontSizeTitle');
-bold.title = browser.i18n.getMessage('boldTitle') + ' (' + userOSKey + '+B)';
-italic.title = browser.i18n.getMessage('italicTitle') + ' (' + userOSKey + '+I)';
-strike.title = browser.i18n.getMessage('strikethroughTitle') + ' (' + userOSKey + '+Shift+' + bindings.strike.key + ')';
-ordered.title = browser.i18n.getMessage('numberedListTitle') + ' (' + userOSKey + '+Shift+' + bindings.ordered.key + ')';
-bullet.title = browser.i18n.getMessage('bulletedListTitle') + ' (' + userOSKey + '+Shift+' + bindings.bullet.key + ')';
-qlDirection.title = browser.i18n.getMessage('textDirectionTitle');
-
-const INITIAL_CONTENT = {
-  ops: [
-    { attributes: { size: 'large', bold: true }, insert: browser.i18n.getMessage('welcomeTitle2') },
-    { insert: '\n\n', attributes: { direction: LANG_DIR, align: TEXT_ALIGN_DIR }},
-    {
-      attributes: { size: 'large' },
-      insert:
-      browser.i18n.getMessage('welcomeText2')
-    },
-    { insert: '\n\n', attributes: { direction: LANG_DIR, align: TEXT_ALIGN_DIR }}
-  ]
-};
-
-let ignoreNextLoadEvent = false;
-function handleLocalContent(content) {
-  if (!content) {
-    browser.storage.local.get('notes').then((data) => {
-      if (!data.hasOwnProperty('notes')) {
-        quill.setContents(INITIAL_CONTENT);
-        ignoreNextLoadEvent = true;
-      } else {
-        quill.setContents(data.notes);
-        chrome.runtime.sendMessage({
-          action: 'kinto-save',
-          content: data.notes
-        }).then(() => {
-          // Clean-up
-          browser.storage.local.remove('notes');
-        });
-      }
-    });
-  } else {
-    if (JSON.stringify(quill.getContents()) !== JSON.stringify(content)) {
-      quill.setContents(content);
-    }
-  }
-  quill.focus();
-}
-
-function loadContent() {
-  ignoreNextLoadEvent = true;
-  chrome.runtime.sendMessage({
-    action: 'kinto-load'
-  });
-}
-
-loadContent();
-
-// Sidebar and background already know about that change.
-quill.on('text-change', () => {
-  const content = quill.getContents();
-  if (!ignoreNextLoadEvent) {
-    // Debounce this second event
-    chrome.runtime.sendMessage({
-      action: 'kinto-save',
-      content
-    });
-
-    // Debounce this second event
-    chrome.runtime.sendMessage({
-      action: 'metrics-changed',
-      context: getPadStats()
-    });
-  } else {
-    ignoreNextLoadEvent = false;
-  }
-});
 
 const footerButtons = document.getElementById('footer-buttons');
 const enableSync = document.getElementById('enable-sync');
@@ -320,31 +11,111 @@ const giveFeedbackButton = document.getElementById('give-feedback-button');
 const noteDiv = document.getElementById('sync-note');
 const syncNoteBody = document.getElementById('sync-note-dialog');
 const closeButton = document.getElementById('close-button');
+
 let isAuthenticated = false;
 enableSync.setAttribute('title', browser.i18n.getMessage('syncNotes'));
 syncNoteBody.textContent = browser.i18n.getMessage('syncNotReady2');
-
 giveFeedbackButton.setAttribute('title', browser.i18n.getMessage('feedback'));
-giveFeedbackButton.addEventListener('click', () => {
-  if (footerButtons.classList.contains('savingLayout')) {
-    browser.tabs.create({
-      active: true,
-      url: SURVEY_PATH
+giveFeedbackButton.setAttribute('href', SURVEY_PATH);
+
+ClassicEditor.create(document.querySelector('#editor'), {
+  heading: {
+    options: [
+      { modelElement: 'paragraph', title: 'P', class: 'ck-heading_paragraph' },
+      { modelElement: 'heading1', viewElement: 'h1', title: 'H1', class: 'ck-heading_heading1' },
+      { modelElement: 'heading2', viewElement: 'h2', title: 'H2', class: 'ck-heading_heading2' },
+      { modelElement: 'heading3', viewElement: 'h3', title: 'H3', class: 'ck-heading_heading3' }
+    ]
+  },
+  toolbar: ['headings', 'bold', 'italic', 'strike', 'bulletedList', 'numberedList'],
+}).then(editor => {
+  return migrationCheck(editor)
+    .then(() => {
+      let ignoreNextLoadEvent = false;
+
+      editor.document.on('change', () => {
+        const content = editor.getData();
+
+        if (!ignoreNextLoadEvent) {
+          // Debounce this second event
+          chrome.runtime.sendMessage({
+            action: 'kinto-save',
+            content
+          });
+
+          // Debounce this second event
+          chrome.runtime.sendMessage({
+            action: 'metrics-changed',
+            context: getPadStats(editor)
+          });
+        } else {
+          ignoreNextLoadEvent = false;
+        }
+      });
+
+      enableSync.onclick = () => {
+        enableSyncAction(editor);
+      };
+
+      loadContent(editor)
+        .then(() => {
+          document.getElementById('loading').style.display = 'none';
+        });
+
+      chrome.runtime.onMessage.addListener(eventData => {
+        switch (eventData.action) {
+          case 'text-change':
+            ignoreNextLoadEvent = true;
+            loadContent(editor);
+            break;
+        }
+      });
+      // Disable right clicks
+      // Refs: https://stackoverflow.com/a/737043/186202
+      document.querySelectorAll('.ck-toolbar, #footer-buttons').forEach((sel) => {
+        sel.addEventListener('contextmenu', e => {
+          e.preventDefault();
+        });
+      });
+
+      localizeEditorButtons();
     });
-  }
+
+}).catch(error => {
+  console.error(error);
 });
+
+
+let ignoreNextLoadEvent = false;
+function handleLocalContent(editor, data) {
+  if (!data.hasOwnProperty('notes2')) {
+    editor.setData(`## ${browser.i18n.getMessage('welcomeTitle2')}\n\n${browser.i18n.getMessage('welcomeText2')}`);
+  } else {
+    if (JSON.stringify(editor.getData()) !== JSON.stringify(data.notes2)) {
+      editor.setData(data.notes2);
+      browser.runtime.sendMessage({
+        action: 'kinto-save',
+        content: data.notes
+      }).then(() => {
+        // Clean-up
+        browser.storage.local.remove('notes2');
+      });
+    }
+  }
+}
+
+function loadContent(editor) {
+  return new Promise((resolve) => {
+    browser.storage.local.get(['notes2'], data => {
+      // If we have a bearer, we try to save the content.
+      handleLocalContent(editor, data);
+      resolve();
+    });
+  });
+}
 
 const savingIndicator = document.getElementById('saving-indicator');
 savingIndicator.textContent = browser.i18n.getMessage('changesSaved');
-
-const giveFeedback = document.getElementById('give-feedback');
-giveFeedback.textContent = browser.i18n.getMessage('feedback');
-giveFeedback.addEventListener('click', () => {
-  browser.tabs.create({
-    active: true,
-    url: SURVEY_PATH
-  });
-});
 
 const disconnectSync = document.getElementById('disconnect-from-sync');
 disconnectSync.style.display = 'none';
@@ -410,14 +181,17 @@ function setAnimation( animateSyncIcon = true, syncingLayout, warning ) { // ani
 
 let loginTimeout;
 let editingInProcess = false;
-enableSync.onclick = () => {
+
+
+function enableSyncAction(editor) {
   if (editingInProcess) {
     return;
   }
+
   if (isAuthenticated && footerButtons.classList.contains('syncingLayout')) {
     // Trigger manual sync
     setAnimation(true);
-    chrome.runtime.sendMessage({
+    browser.runtime.sendMessage({
         action: 'kinto-load'
       });
   } else if (!isAuthenticated && footerButtons.classList.contains('savingLayout')) {
@@ -436,10 +210,10 @@ enableSync.onclick = () => {
 
     browser.runtime.sendMessage({
       action: 'authenticate',
-      context: getPadStats()
+      context: getPadStats(editor)
     });
   }
-};
+}
 
 // gets the user-selected theme from local storage and applies respective CSS
 // file to the document
@@ -495,7 +269,7 @@ chrome.runtime.onMessage.addListener(eventData => {
       // set title attr of footer to the currently logged in account
       footerButtons.title = eventData.profile && eventData.profile.email;
       savingIndicator.textContent = browser.i18n.getMessage('syncProgress');
-      chrome.runtime.sendMessage({
+      browser.runtime.sendMessage({
           action: 'kinto-load'
         });
       break;
@@ -530,7 +304,7 @@ chrome.runtime.onMessage.addListener(eventData => {
       browser.storage.local.set({ last_modified: eventData.last_modified})
         .then(() => {
           getLastSyncedTime();
-          chrome.runtime.sendMessage('notes@mozilla.com', {
+          browser.runtime.sendMessage('notes@mozilla.com', {
             action: 'text-change'
           });
         });
@@ -554,50 +328,50 @@ chrome.runtime.onMessage.addListener(eventData => {
   }
 });
 
-// disable drop of links and images into notes
-const qlEditor = document.querySelectorAll('.ql-editor');
+function getPadStats(editor) {
+  const text = editor.getData();
 
-document.addEventListener('dragover', () => {
-  qlEditor[0].classList.add('forbid-cursor');
-  browser.runtime.sendMessage({
-    action: 'metrics-drag-n-drop',
-    context: getPadStats()
-  });
-  return true;
-});
-
-document.addEventListener('dragleave', () => {
-  qlEditor[0].classList.remove('forbid-cursor');
-  return true;
-});
-
-document.addEventListener('drop', (e) => {
-  e.preventDefault();
-  qlEditor[0].classList.remove('forbid-cursor');
-  return false;
-});
-
-function getPadStats() {
-  const content = quill.getContents();
-  const text = quill.getText();
   const styles = {
     size: false,
     bold: false,
-
     italic: false,
     strike: false,
-    list: false
+    list: false,
+    list_bulleted: false,
+    list_numbered: false
   };
 
-  content.forEach(node => {
-    if (node.hasOwnProperty('attributes')) {
-      Object.keys(node.attributes).forEach(key => {
-        if (styles.hasOwnProperty(key)) {
-          styles[key] = true;
-        }
-      });
+  const range = ClassicEditor.imports.range.createIn( editor.document.getRoot() );
+
+  for ( const value of range ) {
+    if (value.type === 'text') {
+      // Bold
+      if (value.item.textNode._attrs.get('bold')) {
+        styles.bold = true;
+      }
+      // Italic
+      if (value.item.textNode._attrs.get('italic')) {
+        styles.italic = true;
+      }
     }
-  });
+
+    if (value.type === 'elementStart') {
+      // Size
+      if (value.item.name.indexOf('heading') === 0) {
+        styles.size = true;
+      }
+
+      // List
+      if (value.item.name === 'listItem') {
+        styles.list = true;
+        if (value.item._attrs.get('type') === 'bulleted') {
+          styles.list_bulleted = true;
+        } else if (value.item._attrs.get('type') === 'numbered') {
+          styles.list_numbered = true;
+        }
+      }
+    }
+  }
 
   return {
     syncEnabled: false,
@@ -611,17 +385,36 @@ function getPadStats() {
   };
 }
 
+function localizeEditorButtons () {
+  // Clear CKEditor tooltips. Fixes: https://github.com/mozilla/notes/issues/410
+  document.querySelectorAll('.ck-toolbar .ck-tooltip__text').forEach((sel) => {
+    sel.remove();
+  });
+
+  let userOSKey;
+
+  if (navigator.appVersion.indexOf('Mac') !== -1)
+    userOSKey = '⌘';
+  else
+    userOSKey = 'Ctrl';
+
+  const size = document.querySelector('button.ck-button:nth-child(1)'),
+    // Need to target buttons by index. Ref: https://github.com/ckeditor/ckeditor5-basic-styles/issues/59
+    bold = document.querySelector('button.ck-button:nth-child(2)'),
+    italic = document.querySelector('button.ck-button:nth-child(3)'),
+    strike = document.querySelector('button.ck-button:nth-child(4)'),
+    ordered = document.querySelector('button.ck-button:nth-child(5)'),
+    bullet = document.querySelector('button.ck-button:nth-child(6)');
+
+// Setting button titles in place of tooltips
+  size.title = browser.i18n.getMessage('fontSizeTitle');
+  bold.title = browser.i18n.getMessage('boldTitle') + ' (' + userOSKey + '+B)';
+  italic.title = browser.i18n.getMessage('italicTitle') + ' (' + userOSKey + '+I)';
+  strike.title = browser.i18n.getMessage('strikethroughTitle');
+  ordered.title = browser.i18n.getMessage('numberedListTitle');
+  bullet.title = browser.i18n.getMessage('bulletedListTitle');
+}
+
 // Create a connection with the background script to handle open and
 // close events.
 browser.runtime.connect();
-
-
-// Disable right clicks
-// Refs: https://stackoverflow.com/a/737043/186202
-document.getElementById('toolbar').addEventListener('contextmenu', e => {
-  e.preventDefault();
-});
-
-document.getElementById('footer-buttons').addEventListener('contextmenu', e => {
-  e.preventDefault();
-});
