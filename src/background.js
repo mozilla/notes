832de9396637a88beb0b14236e4bc8cc66ea753f
vladikoff <vlad.filippov@gmail.com>
2017-11-21 23:26:53 -0500

    Use latest key server and latest lib

--- src/background.js
+++ src/background.js
@@ -6,8 +6,8 @@ const TRACKING_ID = 'UA-35433268-79';
 const KINTO_SERVER = 'https://kinto.dev.mozaws.net/v1';
 // XXX: Read this from Kinto fxa-params
 const FXA_CLIENT_ID = 'c6d74070a481bc10';
-const FXA_OAUTH_SERVER = 'https://oauth-scoped-keys-oct10.dev.lcip.org/v1';
-const FXA_PROFILE_SERVER = 'https://scoped-keys-oct10.dev.lcip.org/profile/v1';
+const FXA_OAUTH_SERVER = 'https://oauth-latest-keys.dev.lcip.org/v1';
+const FXA_PROFILE_SERVER = 'https://latest-keys.dev.lcip.org/profile/v1';
 const FXA_SCOPES = ['profile', 'https://identity.mozilla.org/apps/notes'];
 const timeouts = {};
 
