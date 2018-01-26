303cd4111e2cf7216eb7b0252cc86b9696964597
Rémy HUBSCHER <rhubscher@mozilla.com>
2018-01-26 11:40:47 +0100

    Use Body text instead of Body @rfeeley review.

--- src/sidebar/panel.js
+++ src/sidebar/panel.js
@@ -45,7 +45,7 @@ ClassicEditor.create(document.querySelector('#editor'), {
       { modelElement: 'heading1', viewElement: 'h1', title: browser.i18n.getMessage('title1'), class: 'ck-heading_heading1' },
       { modelElement: 'heading2', viewElement: 'h2', title: browser.i18n.getMessage('title2'), class: 'ck-heading_heading2' },
       { modelElement: 'heading3', viewElement: 'h3', title: browser.i18n.getMessage('title3'), class: 'ck-heading_heading3' },
-      { modelElement: 'paragraph', title: browser.i18n.getMessage('paragraph'), class: 'ck-heading_paragraph' }
+      { modelElement: 'paragraph', title: browser.i18n.getMessage('bodyText'), class: 'ck-heading_paragraph' }
     ]
   },
   toolbar: ['headings', 'bold', 'italic', 'strike', 'bulletedList', 'numberedList'],
