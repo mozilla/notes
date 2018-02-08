a12c96cd4879a0e94203d1896a9b5fa01554625f
Sébastien BARBIER <contact@sebastienbarbier.com>
2018-02-08 11:12:20 +0700

    Reformat INITIAL_CONTENT in editor

--- src/sidebar/app/components/Editor.js
+++ src/sidebar/app/components/Editor.js
@@ -2,49 +2,35 @@ import React from 'react';
 
 import { getPadStats, customizeEditor } from '../editor.js';
 
-const INITIAL_CONTENT = `<h2>${browser.i18n.getMessage(
-  'welcomeTitle3'
-)}</h2><p>${browser.i18n.getMessage(
-  'welcomeSubtitle'
-)}</p><p><strong>${browser.i18n.getMessage(
-  'welcomeOpenNotes'
-)}</strong></p><ul><li>${browser.i18n.getMessage(
-  'welcomeWindowsLinuxShortcut',
-  '<code>Alt+Shift+W</code>'
-)}</li><li>${browser.i18n.getMessage(
-  'welcomeMacShortcut',
-  '<code>Opt+Shift+W</code>'
-)}</li></ul><p><strong>${browser.i18n.getMessage(
-  'welcomeAccessNotes'
-)}</strong></p><ul><li>${browser.i18n.getMessage(
-  'welcomeSyncInfo',
-  '<strong>' + browser.i18n.getMessage('syncNotes') + '</strong>'
-)}</li></ul><p>${browser.i18n.getMessage(
-  'welcomeFormatText'
-)}</p><ul><li>${browser.i18n
-  .getMessage('welcomeHeading')
-  .replace(/ `/g, ' <code>')
-  .replace(/`/g, '</code>')}</li><li>${browser.i18n
-  .getMessage('welcomeBold')
-  .replace(/ `/g, ' <code>')
-  .replace(/`/g, '</code>')}</li><li>${browser.i18n
-  .getMessage('welcomeItalics')
-  .replace(/ `/g, ' <code>')
-  .replace(/`/g, '</code>')}</li><li>${browser.i18n
-  .getMessage('welcomeBulleted')
-  .replace(/ `/g, ' <code>')
-  .replace(/`/g, '</code>')}</li><li>${browser.i18n
-  .getMessage('welcomeNumbered')
-  .replace(/ `/g, ' <code>')
-  .replace(/`/g, '</code>')}</li><li>${browser.i18n
-  .getMessage('welcomeCode')
-  .replace(/ ``/g, ' <code>`')
-  .replace(/``/g, '`</code>')}</li></ul><p><strong>${browser.i18n.getMessage(
-  'welcomeSuggestion'
-)}</strong></p><ul><li>${browser.i18n.getMessage(
-  'welcomeGiveFeedback',
-  '<strong>' + browser.i18n.getMessage('feedback') + '</strong>'
-)}</li></ul><p>${browser.i18n.getMessage('welcomeThatsIt')}</p>`;
+const INITIAL_CONTENT = `
+  <h2>${browser.i18n.getMessage('welcomeTitle3')}</h2>
+  <p>${browser.i18n.getMessage('welcomeSubtitle')}</p>
+  <p><strong>${browser.i18n.getMessage('welcomeOpenNotes')}</strong></p>
+  <ul>
+    <li>${browser.i18n.getMessage('welcomeWindowsLinuxShortcut','<code>Alt+Shift+W</code>')}</li>
+    <li>${browser.i18n.getMessage('welcomeMacShortcut','<code>Opt+Shift+W</code>')}</li>
+  </ul>
+  <p><strong>${browser.i18n.getMessage('welcomeAccessNotes')}</strong></p>
+  <ul>
+    <li>
+      ${browser.i18n.getMessage('welcomeSyncInfo', '<strong>' + browser.i18n.getMessage('syncNotes') + '</strong>')}
+    </li>
+  </ul>
+  <p>${browser.i18n.getMessage('welcomeFormatText')}</p>
+  <ul>
+    <li>${browser.i18n.getMessage('welcomeHeading').replace(/ `/g, ' <code>').replace(/`/g, '</code>')}</li>
+    <li>${browser.i18n.getMessage('welcomeBold').replace(/ `/g, ' <code>').replace(/`/g, '</code>')}</li>
+    <li>${browser.i18n.getMessage('welcomeItalics').replace(/ `/g, ' <code>').replace(/`/g, '</code>')}</li>
+    <li>${browser.i18n.getMessage('welcomeBulleted').replace(/ `/g, ' <code>').replace(/`/g, '</code>')}</li>
+    <li>${browser.i18n.getMessage('welcomeNumbered').replace(/ `/g, ' <code>').replace(/`/g, '</code>')}</li>
+    <li>${browser.i18n.getMessage('welcomeCode').replace(/ ``/g, ' <code>`').replace(/``/g, '`</code>')}</li>
+  </ul>
+  <p><strong>${browser.i18n.getMessage('welcomeSuggestion')}</strong></p>
+  <ul>
+    <li>${browser.i18n.getMessage('welcomeGiveFeedback','<strong>' + browser.i18n.getMessage('feedback') + '</strong>')}</li>
+  </ul>
+  <p>${browser.i18n.getMessage('welcomeThatsIt')}</p>
+`;
 
 class Editor extends React.Component {
   constructor(props) {
@@ -219,10 +205,10 @@ class Editor extends React.Component {
   render() {
     return (
       <div
+        id="editor"
         ref={node => {
           this.node = node;
         }}
-        id="editor"
       />
     );
   }
