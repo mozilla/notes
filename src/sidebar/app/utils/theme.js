6efb2f62983392508677963d01cbba41fcdfef80
Sébastien BARBIER <contact@sebastienbarbier.com>
2018-02-26 11:40:04 +0100

    WIP

--- src/sidebar/app/utils/theme.js
+++ src/sidebar/app/utils/theme.js
@@ -1,9 +1,16 @@
 // gets the user-selected theme from local storage and applies respective CSS
 // file to the document
+//
 function getThemeFromStorage() {
   const getting = browser.storage.local.get(['theme']);
   getting.then(function applyTheme(data) {
     if (data.theme === 'dark') {
+
+      if (document.getElementsByTagName('body').classList) {
+        document.getElementsByTagName('body').classList.remove('lightTheme');
+        document.getElementsByTagName('body').classList.add('darkTheme');
+      }
+
       if (!document.getElementById('dark-styles')) {
         const darkSS = document.createElement('link');
         darkSS.id = 'dark-styles';
@@ -13,6 +20,12 @@ function getThemeFromStorage() {
         document.getElementsByTagName('head')[0].appendChild(darkSS);
       }
     } else if (data.theme === 'default' || data.theme === undefined) {
+
+      if (document.getElementsByTagName('body').classList) {
+        document.getElementsByTagName('body').classList.remove('darkTheme');
+        document.getElementsByTagName('body').classList.add('lightTheme');
+      }
+
       if (document.getElementById('dark-styles')) {
         const darkSS = document.getElementById('dark-styles');
         darkSS.parentElement.removeChild(darkSS);
