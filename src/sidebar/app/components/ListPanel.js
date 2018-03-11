c7d441374d4edb7d82cb5a38e16d5b601fe85f4b
Sébastien BARBIER <contact@sebastienbarbier.com>
2018-03-11 19:39:22 +0700

    Implement new note button in ListView

--- src/sidebar/app/components/ListPanel.js
+++ src/sidebar/app/components/ListPanel.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import NewIcon from './icons/NewIcon';
+
+const styles = {
+
+};
 
 class ListPanel extends React.Component {
 
@@ -12,6 +17,11 @@ class ListPanel extends React.Component {
   render() {
     return (
       <div>
+        <Link to="/note"
+          className="btn fullWidth borderBottom"
+          title="New note">
+          <NewIcon /> <span>New Note</span>
+        </Link>
         <h1>List panel</h1>
         <Link to="/note">Go to note editor</Link>
       </div>
