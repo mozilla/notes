70248fd920bee7064a2899978b363492eb241160
vladikoff <vlad.filippov@gmail.com>
2018-03-15 15:16:56 -0400

    Update to CKEditor Beta 1
    Fixes https://github.com/mozilla/notes/issues/773

--- ckeditor-build/webpack.config.js
+++ ckeditor-build/webpack.config.js
@@ -9,9 +9,10 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const {bundler} = require('@ckeditor/ckeditor5-dev-utils');
+const {bundler, styles} = require('@ckeditor/ckeditor5-dev-utils');
 const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
 const BabiliPlugin = require('babel-minify-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -29,7 +30,7 @@ module.exports = {
 
   plugins: [
     new CKEditorWebpackPlugin({
-      languages: ['en']
+      language: 'en'
     }),
     new BabiliPlugin(null, {
       comments: false
@@ -47,16 +48,22 @@ module.exports = {
         use: ['raw-loader']
       },
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: [
-          'style-loader',
           {
-            loader: 'css-loader',
+            loader: 'style-loader',
             options: {
-              minimize: true
+              singleton: true
             }
-          },
-          'sass-loader'
+          }, {
+            loader: 'postcss-loader',
+            options: styles.getPostCssConfig({
+              themeImporter: {
+                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+              },
+              minify: false
+            })
+          }
         ]
       }
     ]
--- src/sidebar/app/data/editorConfig.js
+++ src/sidebar/app/data/editorConfig.js
@@ -1,17 +1,17 @@
 const config = {
   'heading': {
     'options': [
-      { modelElement: 'heading1', viewElement: 'h1', title: browser.i18n.getMessage('title1'), class: 'ck-heading_heading1' },
-      { modelElement: 'heading2', viewElement: 'h2', title: browser.i18n.getMessage('title2'), class: 'ck-heading_heading2' },
-      { modelElement: 'heading3', viewElement: 'h3', title: browser.i18n.getMessage('title3'), class: 'ck-heading_heading3' },
-      { modelElement: 'paragraph', title: browser.i18n.getMessage('paragraph'), class: 'ck-heading_paragraph' }
+      { model: 'heading1', view: 'h1', title: browser.i18n.getMessage('title1'), class: 'ck-heading_heading1' },
+      { model: 'heading2', view: 'h2', title: browser.i18n.getMessage('title2'), class: 'ck-heading_heading2' },
+      { model: 'heading3', view: 'h3', title: browser.i18n.getMessage('title3'), class: 'ck-heading_heading3' },
+      { model: 'paragraph', title: browser.i18n.getMessage('paragraph'), class: 'ck-heading_paragraph' }
     ]
   },
-  'toolbar': [
-    'headings',
+  toolbar: [
+    'heading',
     'bold',
     'italic',
-    'strike',
+    'strikethrough',
     'bulletedList',
     'numberedList'
   ]
