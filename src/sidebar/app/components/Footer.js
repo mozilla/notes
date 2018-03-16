f069996bd3371b12da87a2948b761d0c1cd4b746
Cedricium <amaya.cedric@gmail.com>
2018-03-15 17:56:13 -0700

    Blur context menu button when menu closes

--- src/sidebar/app/components/Footer.js
+++ src/sidebar/app/components/Footer.js
@@ -157,6 +157,8 @@ class Footer extends React.Component {
     this.onCloseListener = () => {
       this.menu.classList.replace('open', 'close');
       window.removeEventListener('keydown', this.handleKeyPress);
+      // Blur `this.contextMenuBtn` when context menu closes - fixes #770
+      this.contextMenuBtn.blur();
     };
 
     // Open and close menu
@@ -354,6 +356,7 @@ class Footer extends React.Component {
 
           <div className="photon-menu close top left" ref={menu => this.menu = menu }>
             <button
+              ref={contextMenuBtn => this.contextMenuBtn = contextMenuBtn}
               id="context-menu-button"
               onClick={(e) => this.toggleMenu(e)}
               onKeyDown={this.handleKeyPress}>
