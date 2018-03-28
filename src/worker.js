913a8766859901c8ae5f923b8861787ea6b5462d
Sébastien BARBIER <contact@sebastienbarbier.com>
2018-03-28 13:49:18 +0700

    Fix linting

--- src/worker.js
+++ src/worker.js
@@ -4,10 +4,10 @@ onmessage = function(eventData) {
   // Action object is the on generated in action object
   // const action = event.data;
 
-  switch (eventData.action) {
-    case 'some-action':
-      break;
-    default:
-      return;
+  // switch (eventData.action) {
+  //   case 'some-action':
+  //     break;
+  //   default:
+  //     return;
   }
 };
