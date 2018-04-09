7197d01146e3b6d87eb8fd30b73a92dfaf26fd8b
Vlad Filippov <vlad.filippov@gmail.com>
2018-04-08 22:00:07 -0400

    Add Kinto to mobile app. (#914)

--- native/app/components/ListItem.js
+++ native/app/components/ListItem.js
@@ -7,12 +7,29 @@ import {
 
 const striptags = require('striptags');
 
+function formatLastModified(date) {
+
+  if (new Date().getDate() === date.getDate()) {
+    return date.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+  }
+
+  return date.toLocaleDateString([], {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+}
+
 export default class ListItem extends React.Component {
   render() {
     const {
       content,
       navigate,
-      rowId
+      rowId,
+      lastModified
     } = this.props;
 
 
@@ -25,6 +42,9 @@ export default class ListItem extends React.Component {
           <View style={{ padding: 10 }} >
             <Text>
               {striptags(content).substr(0, 50)}
+            </Text>
+            <Text>
+              {formatLastModified(lastModified)}
             </Text>
           </View>
         </View>
--- native/app/components/LoginPanel.js
+++ native/app/components/LoginPanel.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import { View, Text, ToastAndroid, Image } from 'react-native';
-import { Button } from 'react-native-paper';
-import { connect } from 'react-redux';
+import fxaUtils from '../vendor/fxa-utils';
+import kintoClient from '../vendor/kinto-client';
 import PropTypes from 'prop-types';
-import store from '../store';
+import React from 'react';
+import sync from '../sync';
+import { Button } from 'react-native-paper';
+import { COLOR_NOTES_BLUE } from '../utils/constants';
+import { connect } from 'react-redux';
 import { trackEvent } from '../utils/metrics';
-import { authorize, refresh } from 'react-native-app-auth';
-import * as Keychain from 'react-native-keychain';
-import {
-  COLOR_NOTES_BLUE
-} from '../utils/constants';
+import { View, Text, ToastAndroid, Image } from 'react-native';
 
-import { createNote } from '../actions';
-
-const base64url = require('../vendor/base64url');
-const fxaCryptoRelier = require('../vendor/fxa-crypto-relier');
-const fxaUtils = require('../vendor/fxa-utils');
-const kintoUtils = require('../vendor/kinto-utils');
-
-const FXA_PROFILE_SERVER = 'https://featurebox.dev.lcip.org/profile/v1';
 const FXA_OAUTH_SERVER = 'https://oauth-featurebox.dev.lcip.org/v1';
 const OAUTH_CLIENT_ID = 'b7d74070a481bc11';
 const OAUTH_REDIRECT = 'https://mozilla-lockbox.github.io/fxa/ios-redirect.html';
@@ -26,9 +16,7 @@ const OAUTH_SCOPES = ['profile', 'https://identity.mozilla.com/apps/notes'];
 
 const refreshConfig = {
   serviceConfiguration: {
-    //authorizationEndpoint: 'https://oauth.accounts.firefox.com/v1/authorization',
     authorizationEndpoint: `${FXA_OAUTH_SERVER}/authorization`,
-    //tokenEndpoint: 'https://oauth.accounts.firefox.com/v1/token',
     tokenEndpoint: `${FXA_OAUTH_SERVER}/token`
   },
   clientId: OAUTH_CLIENT_ID,
@@ -36,83 +24,19 @@ const refreshConfig = {
   scopes: OAUTH_SCOPES
 };
 
-const fxaKeyUtils = new fxaCryptoRelier.KeyUtils();
-
 class LoginPanel extends React.Component {
   onAuth (options={}) {
     this.props.navigation.navigate('LoadingPanel');
 
-    let oauthResponse;
-    let scopedKeys;
-
     return Promise.resolve().then(() => {
-      if (options.oauthResponse) {
-        console.log('Using oauthResponse:', options.oauthResponse);
-
-        return options.oauthResponse;
-      } else {
-        return fxaKeyUtils.createApplicationKeyPair().then((keyTypes) => {
-          const base64JwkPublicKey = base64url.encode(JSON.stringify(keyTypes.jwkPublicKey), 'utf8');
-          const config = {
-            serviceConfiguration: {
-              //authorizationEndpoint: 'https://oauth.accounts.firefox.com/v1/authorization',
-              authorizationEndpoint: `${FXA_OAUTH_SERVER}/authorization`,
-              //tokenEndpoint: 'https://oauth.accounts.firefox.com/v1/token',
-              tokenEndpoint: `${FXA_OAUTH_SERVER}/token`
-            },
-            additionalParameters: {
-              keys_jwk: base64JwkPublicKey,
-              access_type: 'offline'
-            },
-            clientId: OAUTH_CLIENT_ID,
-            redirectUrl: OAUTH_REDIRECT,
-            scopes: OAUTH_SCOPES
-          };
-
-          return authorize(config);
-        });
-      }
-    }).then((response) => {
-      oauthResponse = response;
-      const bundle = oauthResponse.additionalParameters.keys_jwe;
-      console.log('oauthResponse', oauthResponse);
-      console.log('keys_jwe', bundle);
-
+      return fxaUtils.launchOAuthKeyFlow();
+    }).then((loginDetails) => {
       trackEvent('login-success');
 
-      return fxaKeyUtils.decryptBundle(bundle);
-    }).then((keys) => {
-      scopedKeys = keys;
-      return fxaUtils.fxaFetchProfile(FXA_PROFILE_SERVER, oauthResponse.accessToken);
-    }).then((profile) => {
-      console.log('profile', profile);
-      Keychain.setGenericPassword(profile.uid, JSON.stringify({
-        profile: profile,
-        oauthResponse: oauthResponse
-      }));
-      ToastAndroid.show('Logged in as ' + profile.email, ToastAndroid.SHORT);
-
-      return kintoUtils.fetchRecords(oauthResponse.accessToken)
-    }).then((records) => {
-      console.log('kinto records', records);
-      return kintoUtils.decryptRecords(records, scopedKeys)
-    }).then((dec) => {
-      console.log('decrypted records', dec);
-
-      const newNotes = [];
-      dec.forEach((note) => {
-        newNotes.push(store.dispatch(createNote(note.content)));
-      });
-
-      return Promise.all(newNotes);
+      ToastAndroid.show('Logged in as ' + loginDetails.profile.email, ToastAndroid.SHORT);
+      return sync.loadFromKinto(kintoClient, loginDetails);
     }).then(() => {
       this.props.navigation.navigate('ListPanel');
-
-      return refresh(refreshConfig, {
-        refreshToken: oauthResponse.refreshToken
-      });
-    }).then((refreshResult) => {
-      console.log('refreshResult', refreshResult);
     }).catch((err) => {
       this.props.navigation.navigate('LoginPanel');
       console.log('onAuth', err);
@@ -122,26 +46,6 @@ class LoginPanel extends React.Component {
   }
 
   render() {
-    try {
-      // Retrieve the credentials
-      Keychain.getGenericPassword().then((credentials) => {
-        if (credentials) {
-          console.log('Credentials successfully loaded for user ' + credentials.username);
-          const oauthResponse = JSON.parse(credentials.password).oauthResponse;
-          console.log(oauthResponse);
-          // return this.onAuth({
-          //   oauthResponse: oauthResponse
-          // });
-        } else {
-          console.log('No credentials stored')
-        }
-
-      });
-    } catch (error) {
-      // TODO: What on earth do we do here?
-      console.log('Keychain cannot be accessed!', error);
-    }
-
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Image
