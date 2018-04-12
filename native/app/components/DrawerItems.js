f0100d0e2cf821100e863905687dbf72c0dc44f1
Sébastien BARBIER <contact@sebastienbarbier.com>
2018-04-12 10:36:28 +0700

    Design static "last synced" footer in the navigation drawer
    No animation or action yet.
    
    Related to #887

--- native/app/components/DrawerItems.js
+++ native/app/components/DrawerItems.js
@@ -5,18 +5,22 @@ import PropTypes from 'prop-types';
 import store from "../store";
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
-import { Title, Text } from 'react-native-paper';
-import { View, StyleSheet, Image, Linking } from 'react-native';
+import { Title, Text, TouchableRipple } from 'react-native-paper';
+
+import { View, ScrollView, StyleSheet, Image, Linking, Modal } from 'react-native';
+
 import {COLOR_NOTES_BLUE} from "../utils/constants";
 import {DrawerItem, DrawerSection, Colors } from 'react-native-paper';
 import {trackEvent} from "../utils/metrics";
 
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 // Url to open to give feedback
 const SURVEY_PATH = 'https://qsurvey.mozilla.com/s3/notes?ref=android';
 const DrawerItemsData = [
-  { label: 'Notes', icon: 'folder', key: 0 },
-  { label: 'Give Feedback', icon: 'message', key: 1 },
-  { label: 'Log out', icon: 'exit-to-app', key: 2 },
+  { label: 'Give Feedback', key: 0 },
+  { label: 'Log out', key: 1 },
 ];
 
 class DrawerItems extends React.Component {
@@ -36,15 +40,15 @@ class DrawerItems extends React.Component {
 
     this.setState({ drawerItemIndex: index });
     // TODO: Refactor this to use something else other than keys?
-    if (key === 0) {
-      return this.props.navigation.navigate('DrawerClose');
-    }
+    // if (key === 0) {
+    //   return this.props.navigation.navigate('DrawerClose');
+    // }
 
-    if (key === 1) {
+    if (key === 0) {
       return Linking.openURL(SURVEY_PATH);
     }
 
-    if (key === 2) {
+    if (key === 1) {
       function navigateToLogin () {
         // Reset back button nav. See https://reactnavigation.org/docs/navigation-actions.html#reset
         const resetAction = NavigationActions.reset({
@@ -64,30 +68,44 @@ class DrawerItems extends React.Component {
 
   render() {
     return (
-      <View style={[styles.drawerContent]}>
-        <View style={{ paddingTop: 55, paddingLeft: 10, paddingBottom: 10, backgroundColor: COLOR_NOTES_BLUE }}>
-        <Image
-          style={{width: 75, height: 75 }}
-          borderRadius={100}
-          borderColor='#FFFFFF'
-          borderWidth={2}
-          resizeMode='cover'
-          source={{uri: this.props.state.sync.avatar}}
-        />
-        <Title style={{ color: '#FFFFFF' }}>{this.props.state.sync.displayName}</Title>
-        <Text style={{ color: '#FFFFFF' }}>{this.props.state.sync.email}</Text>
+      <View style={styles.drawerContent}>
+        <View style={{ paddingTop: 55, paddingLeft: 30, paddingBottom: 20, backgroundColor: COLOR_NOTES_BLUE }}>
+          <Image
+            style={{width: 75, height: 75 }}
+            borderRadius={100}
+            borderColor='#FFFFFF'
+            borderWidth={2}
+            resizeMode='cover'
+            source={{uri: this.props.state.sync.avatar}}
+          />
+          <Title style={{ color: '#FFFFFF' }}>{this.props.state.sync.displayName}</Title>
+          <Text style={{ color: '#FFFFFF' }}>{this.props.state.sync.email}</Text>
         </View>
-        <DrawerSection>
-          {DrawerItemsData.map((props, index) => (
-            <DrawerItem
-              {...props}
-              key={props.key}
-              color={props.key === 3 ? Colors.tealA200 : undefined}
-              //active={this.state.drawerItemIndex === index}
-              onPress={() => this._setDrawerItem(index, props.key)}
-            />
-          ))}
-        </DrawerSection>
+        <ScrollView style={styles.drawerSection}>
+          <DrawerSection>
+            {DrawerItemsData.map((props, index) => (
+              <DrawerItem
+                {...props}
+                style={{ paddingLeft: 14 }}
+                key={props.key}
+                color={props.key === 3 ? Colors.tealA200 : undefined}
+                //active={this.state.drawerItemIndex === index}
+                onPress={() => this._setDrawerItem(index, props.key)}
+              />
+            ))}
+          </DrawerSection>
+
+        </ScrollView>
+        <TouchableRipple style={styles.footer} onPress={() => console.log('was pressed')}>
+          <View style={styles.footerWrapper}>
+              <Text style={{ color: undefined, fontSize: 13 }}>Last synced 9:31 AM</Text>
+              <MaterialIcons
+                name="sync"
+                style={{ marginRight: 10, color: undefined }}
+                size={20}
+              />
+          </View>
+        </TouchableRipple>
       </View>
     );
   }
@@ -97,6 +115,23 @@ const styles = StyleSheet.create({
   drawerContent: {
     flex: 1,
   },
+  drawerSection: {
+    flexGrow: 1,
+    paddingTop: 10
+  },
+  footer: {
+    flexGrow: 0
+  },
+  footerWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexGrow: 0,
+    paddingTop: 15,
+    paddingBottom: 14,
+    paddingLeft: 28,
+    paddingRight: 10
+  }
 });
 
 // export default withTheme(DrawerItems);
