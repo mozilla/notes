7fdbcb36f06407b6309d5b6af5be46a53efb119d
vladikoff <vlad.filippov@gmail.com>
2018-04-13 11:16:19 -0400

    Remove unused supported locales

--- native/misc/config-generator.js
+++ native/misc/config-generator.js
@@ -1,15 +1,6 @@
 const config = require('../config.json');
-
-const { writeFileSync, lstatSync, readdirSync } = require('fs');
-const { join } = require('path');
-
-const isDirectory = source => lstatSync(source).isDirectory();
-const getDirectories = source => {
-  return readdirSync(source).map(name => join(source, name)).filter(isDirectory).map(i => i.split('/')[2]);
-};
+const { writeFileSync } = require('fs');
 
 config['SENTRY_DSN'] = process.env.SENTRY_DSN;
-
-config['SUPPORTED_LOCALES'] = getDirectories('app/_locales');
 
 writeFileSync('config.json', JSON.stringify(config, null, 2));
