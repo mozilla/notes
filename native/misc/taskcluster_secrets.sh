5ac553023d4b6f4a61a5d6ca1af340a93fd579a0
vladikoff <vlad.filippov@gmail.com>
2018-05-15 13:57:35 -0400

    Export token

--- native/misc/taskcluster_secrets.sh
+++ native/misc/taskcluster_secrets.sh
@@ -4,5 +4,7 @@ set +x
 secrets_url="http://taskcluster/secrets/v1/secret/project/testpilot/notes"
 SECRET=$(curl "${secrets_url}")
 echo "${SECRET}"
+TOKEN=$(echo "${SECRET}" | jq -r '.secret.foo')
+echo "${TOKEN}"
 
 set -x
