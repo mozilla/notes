d616055bb6ac1591f82021ad1579d9d375caf166
vladikoff <vlad.filippov@gmail.com>
2018-05-15 14:20:09 -0400

    Pull proper values from taskcluster

--- native/misc/taskcluster_secrets.sh
+++ native/misc/taskcluster_secrets.sh
@@ -3,8 +3,13 @@
 set +x
 secrets_url="http://taskcluster/secrets/v1/secret/project/testpilot/notes"
 SECRET=$(curl "${secrets_url}")
-echo "${SECRET}"
-TOKEN=$(echo "${SECRET}" | jq -r '.secret.foo')
-echo "${TOKEN}"
+
+KEYSTORE=$(echo "${SECRET}" | jq -r '.secret.KEYSTORE')
+KEYSTORE_URI=$(echo "${SECRET}" | jq -r '.secret.KEYSTORE_URI')
+MYAPP_RELEASE_KEY_ALIAS=$(echo "${SECRET}" | jq -r '.secret.MYAPP_RELEASE_KEY_ALIAS')
+MYAPP_RELEASE_KEY_PASSWORD=$(echo "${SECRET}" | jq -r '.secret.MYAPP_RELEASE_KEY_PASSWORD')
+MYAPP_RELEASE_STORE_FILE=$(echo "${SECRET}" | jq -r '.secret.MYAPP_RELEASE_STORE_FILE')
+MYAPP_RELEASE_STORE_PASSWORD=$(echo "${SECRET}" | jq -r '.secret.MYAPP_RELEASE_STORE_PASSWORD')
+SENTRY_DSN=$(echo "${SECRET}" | jq -r '.secret.SENTRY_DSN')
 
 set -x
