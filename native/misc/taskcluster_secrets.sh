5f70a34c1afd1c823c106674c8f24ab043d2ea27
vladikoff <vlad.filippov@gmail.com>
2018-05-15 14:55:29 -0400

    Sigh

--- native/misc/taskcluster_secrets.sh
+++ native/misc/taskcluster_secrets.sh
@@ -13,4 +13,14 @@ export MYAPP_RELEASE_STORE_FILE=$(echo "${SECRET}" | jq -r '.secret.MYAPP_RELEAS
 export MYAPP_RELEASE_STORE_PASSWORD=$(echo "${SECRET}" | jq -r '.secret.MYAPP_RELEASE_STORE_PASSWORD')
 export SENTRY_DSN=$(echo "${SECRET}" | jq -r '.secret.SENTRY_DSN')
 
+# use curl to download a keystore from $KEYSTORE_URI, if set,
+# to the path/filename set in $KEYSTORE.
+if [[ $KEYSTORE && ${KEYSTORE} && $KEYSTORE_URI && ${KEYSTORE_URI} ]]
+then
+    echo "Keystore detected - downloading..."
+    curl -L -o android/app/${KEYSTORE} ${KEYSTORE_URI}
+else
+    echo "Keystore uri not set.  .APK artifact will not be signed."
+fi
+
 set -x
