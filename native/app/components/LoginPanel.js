f3ab3ae98c7743a569eda126350efebbe3aebcb7
Vlad Filippov <vlad.filippov@gmail.com>
2018-07-03 16:16:23 -0400

    Clean up login panel (#1249)

--- native/app/components/LoginPanel.js
+++ native/app/components/LoginPanel.js
@@ -26,23 +26,23 @@ class LoginPanel extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F9F9FA' }}>
-        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 15 }}>
+
+        <View style={{ alignItems: 'center', justifyContent: 'flex-end', paddingTop: 15, alignItems: 'center', justifyContent: 'center', width: '70%' }}>
           <Image
             style={{width: 150, height: 150 }}
             source={require('../assets/notes-1024.png')}
           />
-          <Text style={{ fontWeight: 'bold', fontSize: 24, padding: 10, paddingTop: 30 }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 24, padding: 10, paddingTop: 30, paddingBottom: 30 }}>
             { i18nGetMessage('welcomeTitle4') }
           </Text>
-          <Text style={{ fontWeight: 'bold', fontSize: 12, paddingBottom: 30 }}>{ i18nGetMessage('welcomeHeadline') }</Text>
+          <Text style={{ fontWeight: 'bold', fontSize: 16, paddingBottom: 30, textAlign: 'center', lineHeight: 22 }}>{ i18nGetMessage('welcomeHeadline') }</Text>
           <Button loading={this.state.isLoading} raised onPress={this.onAuth.bind(this)} color={COLOR_NOTES_BLUE}
             style={styles.btnSignin}><Text style={{ fontSize: 14 }}>{ i18nGetMessage('signIn') }</Text></Button>
-        </View>
-        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 15, width: '80%' }}>
-          <Text style={{ fontSize: 12, paddingTop: 20, textAlign: 'center' }}>
+
+          <Text style={{ fontSize: 14, paddingTop: 25, textAlign: 'center', lineHeight: 18, paddingBottom: 10  }}>
             { i18nGetMessage('usageHint') }
           </Text>
-          <Text style={{color: COLOR_NOTES_BLUE, fontSize: 12}}
+          <Text style={{color: COLOR_NOTES_BLUE, fontSize: 14, lineHeight: 18, textDecorationLine: 'underline'}}
                 onPress={() => Linking.openURL('https://testpilot.firefox.com/experiments/notes')}>
             { i18nGetMessage('usageLearnMore') }
           </Text>
@@ -66,6 +66,7 @@ LoginPanel.propTypes = {
 
 const styles = StyleSheet.create({
   btnSignin: {
+    minWidth: 160,
     borderRadius: 25,
     paddingLeft: 15,
     paddingRight: 15,
