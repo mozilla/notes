4691e081d9a8df9f8f96bdf257c00acc274aaab6
Kevin "Ilphrin" Pellet <ilphrin@autistici.org>
2020-02-05 22:20:24 +0100

    Cleanup ListPanel.js

--- native/app/components/ListPanel.js
+++ native/app/components/ListPanel.js
@@ -1,23 +1,18 @@
-import fxaUtils from '../vendor/fxa-utils';
-import kintoClient from '../vendor/kinto-client';
 import ListItem from './ListItem';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { store, persistor } from "../store";
-import sync from "../utils/sync";
+import { persistor } from '../store';
 import { connect } from 'react-redux';
-import { FAB } from 'react-native-paper';
+import { lodash as _ } from 'lodash';
 
 import { View, FlatList, StyleSheet, RefreshControl, AppState, Animated, ToastAndroid } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
-import { COLOR_DARK_SYNC, COLOR_DARK_WARNING, COLOR_NOTES_BLUE, COLOR_NOTES_WHITE, KINTO_LOADED } from '../utils/constants';
-import { kintoLoad, createNote, setNetInfo, authenticate, reconnectSync, openingLogin } from "../actions";
+import { COLOR_NOTES_BLUE } from '../utils/constants';
+import { kintoLoad, setNetInfo } from '../actions';
 import browser from '../browser';
 
 import ListPanelEmpty from './ListPanelEmpty';
 import ListPanelLoading from './ListPanelLoading';
-
-var _ = require('lodash')
 
 class ListPanel extends React.Component {
   constructor(props) {
@@ -69,7 +64,7 @@ class ListPanel extends React.Component {
         persistor.flush();
       }
       this.setState({ appState: nextAppState });
-    }
+    };
 
     this._handleNetworkStateChange = (connectionInfo) => {
       const wasConnected = this.props.state.sync.isConnected;
@@ -97,10 +92,10 @@ class ListPanel extends React.Component {
   shouldComponentUpdate(nextProps) {
     let shouldUpdate = false;
 
-    const changedProps = _.reduce(this.props.state, function (result, value, key) {
+    const changedProps = _.reduce(this.props.state, function(result, value, key) {
       return _.isEqual(value, nextProps.state[key])
         ? result
-        : result.concat(key)
+        : result.concat(key);
     }, []);
 
     // only render when notes collection changes
@@ -121,93 +116,84 @@ class ListPanel extends React.Component {
 
   renderList() {
     const { navigate } = this.props.navigation;
-    if (!this.props.state.kinto.isLoaded) {
-      return (
-        <ListPanelLoading></ListPanelLoading>
-      )
+    let styleList = {};
+    if (this.props.state.notes.length === 0) {
+      styleList = styles.listEmpty;
     } else {
-      let styleList = {};
-      if (this.props.state.notes.length === 0) {
-        styleList = {
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingRight: 40,
-          paddingLeft: 40
-        };
-      } else {
-        styleList = { marginBottom:90 };
-      }
-
-      return (
-        <FlatList
-          contentContainerStyle={styleList}
-          data={this.props.state.notes.sort((a, b) => {
-            const aStamp = new Date(a.lastModified).getTime();
-            const bStamp = new Date(b.lastModified).getTime();
-            return aStamp <= bStamp ? 1 : -1
-          })}
-          refreshControl={
-            <RefreshControl
-              enabled={! this.state.refreshing || ! this.state.sync.selected || ! this.state.sync.isSyncing}
-              refreshing={this.state.refreshing}
-              colors={[ COLOR_NOTES_BLUE ]}
-              onRefresh={this._onRefresh.bind(this)}
-            />
-          }
-          ListEmptyComponent={() => {
-            return (
-              <ListPanelEmpty></ListPanelEmpty>
-            )
-          }}
-          ListHeaderComponent={() => {
-            return this.props.state.notes && this.props.state.notes.length > 0 ?
-              (
-                <View style={{ backgroundColor: 'white', height: 10}}></View>
-              )
-              : null;
-          }}
-          keyExtractor={ (item) => item.id }
-          renderItem={({item}) => {
-            return (
-              <ListItem
-                note={item}
-                navigate={navigate}
-              />
-            )
-          }}
-          ListFooterComponent={() => {
-            return this.props.state.notes && this.props.state.notes.length > 0 ?
-              (
-                <View style={{
-                  height: 1,
-                  backgroundColor: '#F9F9FA',
-                  overflow: 'visible',
-                  marginBottom: 90,
-                  elevation: 1,
-                  shadowColor: '#000',
-                  shadowOpacity: 0.24,
-                  shadowOffset: { width: 0, height: 0.75},
-                  shadowRadius: 1.5}}>
-                </View>
-              )
-              : null;
-          }}
-        />
-      )
+      styleList = styles.list;
     }
+
+    return this.props.state.kinto.isLoaded ? <ListPanelLoading /> : (
+
+      <FlatList
+        contentContainerStyle={styleList}
+        data={this.props.state.notes.sort((a, b) => {
+          const aStamp = new Date(a.lastModified).getTime();
+          const bStamp = new Date(b.lastModified).getTime();
+          return aStamp <= bStamp ? 1 : -1;
+        })}
+        refreshControl={
+          <RefreshControl
+            enabled={!this.state.refreshing || !this.state.sync.selected || !this.state.sync.isSyncing}
+            refreshing={this.state.refreshing}
+            colors={[ COLOR_NOTES_BLUE ]}
+            onRefresh={this._onRefresh.bind(this)}
+          />
+        }
+        ListEmptyComponent={() => {
+          return (
+            <ListPanelEmpty></ListPanelEmpty>
+          );
+        }}
+        ListHeaderComponent={() => {
+          return this.props.state.notes && this.props.state.notes.length > 0 ?
+            (
+              <View style={{ backgroundColor: 'white', height: 10}}></View>
+            )
+            : null;
+        }}
+        keyExtractor={ (item) => item.id }
+        renderItem={({item}) => {
+          return (
+            <ListItem
+              note={item}
+              navigate={navigate}
+            />
+          );
+        }}
+        ListFooterComponent={() => {
+          return this.props.state.notes && this.props.state.notes.length > 0 ?
+            (
+              <View style={styles.footer} />
+            )
+            : null;
+        }}
+      />
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  fab: {
-    width: 56,
-    height: 56,
-    borderRadius: 30,
-    backgroundColor: COLOR_NOTES_BLUE,
-    position: 'absolute',
-    bottom: 24,
-    right: 24,
+  listEmpty: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 40,
+    paddingLeft: 40
+  },
+  list: {
+    marginBottom: 90
+  },
+  footer: {
+    height: 1,
+    backgroundColor: '#F9F9FA',
+    overflow: 'visible',
+    marginBottom: 90,
+    elevation: 1,
+    shadowColor: '#000',
+    shadowOpacity: 0.24,
+    shadowOffset: { width: 0, height: 0.75 },
+    shadowRadius: 1.5
   }
 });
 
@@ -222,4 +208,4 @@ ListPanel.propTypes = {
   dispatch: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps)(ListPanel)
+export default connect(mapStateToProps)(ListPanel);
